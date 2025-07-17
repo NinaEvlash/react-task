e058d242342ecf6b1887bd4f5c3f0ef40ddaed85
Нина Евлаш <evlash.nina@mail.ru>
2025-07-18 01:06:47 +0200

    feat(test): add Jest configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import ErrorBoundary from './components/ErrorBoundary';
 
 const root = document.getElementById('root');
