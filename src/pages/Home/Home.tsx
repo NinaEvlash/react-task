d3506c00cc62c872609b40ac21a4e461014e3570
Нина Евлаш <evlash.nina@mail.ru>
2026-05-19 00:21:30 +0200

    fix: fix bug in fetchData

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -51,7 +51,7 @@ export default function Home() {
             setError(message);
             setLoading(false);
 
-            clearQuery();
+            localStorage.removeItem('pokemonSearchQuery');
             return;
           }
 
