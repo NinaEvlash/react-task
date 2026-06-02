855966bd274bfe7ab39ebd26394dbd7dd74032e0
Нина Евлаш <evlash.nina@mail.ru>
2026-06-02 16:59:36 +0200

    chore: update React to v19 and remove outdated React Router typings

--- src/api/dataApi.ts
+++ src/api/dataApi.ts
@@ -14,7 +14,7 @@ export async function getDataByName(name: string): Promise<PokemonDetailsRespons
     throw new Error('Something went wrong');
   }
 
-  return response.json() as Promise<PokemonDetailsResponse>;
+  return response.json();
 }
 
 export async function getDataList(limit: number, offset: number): Promise<PokemonListResponse> {
@@ -28,5 +28,5 @@ export async function getDataList(limit: number, offset: number): Promise<Pokemo
     throw new Error('Failed to load Pokémon list');
   }
 
-  return response.json() as Promise<PokemonListResponse>;
+  return response.json();
 }
